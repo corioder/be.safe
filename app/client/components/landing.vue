362c787db2fb67631743a4aa6d957f2291575383
artalatarta <artur@mucowski.pl>
2020-10-22 13:32:38 +0200

    mobile landing done

---
+++ app/client/components/landing.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="landing">
+		<h2>
+			Bądź bezpieczny w czasie <span>pandemii <span class="bold">COVID-19</span></span>
+		</h2>
+		<div class="container">
+			<div class="links">
+				<router-link to="/informed" class="informed" tag="button">informacja</router-link>
+				<router-link to="/aware" class="aware" tag="button">świadomość</router-link>
+				<router-link to="/preventive" class="preventive" tag="button">profilaktyka</router-link>
+			</div>
+			<img src="../assets/landing.svg" />
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'landing',
+	};
+</script>
+
+<style lang="scss" scoped>
+	@import '../scss/mixins/_flex.scss';
+	@import '../scss/vars/_colors.scss';
+	.landing {
+		@include flex(column, flex-start);
+		h2 {
+			font-size: 18px;
+			font-weight: 400;
+			width: 250px;
+			.bold {
+				font-weight: 600;
+			}
+		}
+		.container {
+			@include flex(row);
+			.links {
+				@include flex(column, flex-start);
+				button {
+					border: none;
+					padding: 6px;
+					margin: 0;
+					margin-bottom: 9px;
+					font-size: 14px;
+					color: $babyPowder;
+				}
+				.informed {
+					background-color: $orange;
+				}
+				.aware {
+					background-color: $cinnabar;
+				}
+				.preventive {
+					background-color: $shamrockGreen;
+					margin: 0;
+				}
+			}
+			img {
+				margin-left: 28px;
+				width: calc(100vw - 93px - 28px - 24px);
+				max-width: 300px;
+			}
+		}
+	}
+	@media (min-width: 375px) {
+		h2 {
+			margin-bottom: 0;
+		}
+	}
+</style>
--- app/client/views/home.vue
+++ app/client/views/home.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="home">
-    <boxes />
-    <charts />
-  </div>
+	<div class="home">
+		<landing />
+		<boxes />
+		<charts />
+	</div>
 </template>
 
 <script>
-  import boxes from '../components/boxes/boxes.vue';
-  import charts from '../components/charts/charts.vue';
+	import landing from '../components/landing.vue';
+	import boxes from '../components/boxes/boxes.vue';
+	import charts from '../components/charts/charts.vue';
 
-  export default {
-    name: 'home',
-    components: {
-      boxes,
-      charts,
-    },
-  };
+	export default {
+		name: 'home',
+		components: {
+			landing,
+			boxes,
+			charts,
+		},
+	};
 </script>
 
 <style lang="scss" scoped>
-  @import '../scss/mixins/_flex.scss';
-  .home {
-    margin-top: 32px;
-    @include flex(column);
-  }
+	@import '../scss/mixins/_flex.scss';
+	.home {
+		margin-top: 32px;
+		@include flex(column);
+	}
 </style>
