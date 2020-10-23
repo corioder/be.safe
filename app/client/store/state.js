7fb7579adeb2e327c74b278ed94d2ac2faf75f5b
watjurk <watjurk@gmail.com>
2020-10-23 11:13:42 +0200

    config fix

--- app/client/store/state.js
+++ app/client/store/state.js
@@ -1,23 +1,25 @@
 export default {
-  isNavOpened: false,
-  links: [
-    { component: 'home', path: '/' },
-    { component: 'informed', path: '/informed' },
-    { component: 'aware', path: '/aware' },
-    { component: 'preventive', path: '/preventive' },
-  ],
-  APIS: {
-    API: __API__, // webpack will resolve this, use .env API=apiUrl
-    STRAPI: __STRAPI__, // webpack will resolve this, use .env STRAPI=strapiUrl
-  },
-  data: {
-    perday: [],
-    today: {},
-    yesterday: {},
-    common: {},
-    provinces: {},
-    countryperday: {},
-    prognosis: {},
-  },
-  categories: [],
+	isNavOpened: false,
+	links: [
+		{ component: 'home', path: '/' },
+		{ component: 'informed', path: '/informed' },
+		{ component: 'aware', path: '/aware' },
+		{ component: 'preventive', path: '/preventive' },
+	],
+	APIS: {
+		API_ENPOINT: 'api/',
+		MAP_ENPOINT: 'map/',
+		API: __API__, // webpack will resolve this, use .env API=apiUrl
+		STRAPI: __STRAPI__, // webpack will resolve this, use .env STRAPI=strapiUrl
+	},
+	data: {
+		perday: [],
+		today: {},
+		yesterday: {},
+		common: {},
+		provinces: {},
+		countryperday: {},
+		prognosis: {},
+	},
+	categories: [],
 };
