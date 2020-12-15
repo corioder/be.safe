6ab7617e4cbde5bd1d9e8907609fbc72aab885d7
artalatarta <artur@mucowski.pl>
2020-12-15 11:24:09 +0100

    fixed not showing data for day in december

--- app/utils/dateToArrOfStrings.js
+++ app/utils/dateToArrOfStrings.js
@@ -1,13 +1,12 @@
 export default (date) => {
 	const dataDate = new Date(date);
-	dataDate.setMonth(dataDate.getMonth() + 1);
 	const stringDates = [];
 
 	for (let i = 0; i < 3; i++) {
 		dataDate.setDate(dataDate.getDate() - 1);
 		stringDates.push(
 			`${dataDate.getDate() < 10 ? '0' + dataDate.getDate() : dataDate.getDate()}.${
-				dataDate.getMonth() < 10 ? '0' + dataDate.getMonth() : dataDate.getMonth()
+				dataDate.getMonth() + 1 < 10 ? '0' + (dataDate.getMonth() + 1) : dataDate.getMonth() + 1
 			}.${dataDate.getFullYear()}`,
 		);
 	}
